737c0d1fefb1eece3f8c3bbda03438cf46d8ae94
Janis Fliegenschmidt <janisf@gmx.net>
2013-11-21 18:11:49 +0100

    Updated Custom Exception to comply with standards.

--- BrainfuckInterpreter/Core/NoInputSourceAvailableException.cs
+++ BrainfuckInterpreter/Core/NoInputSourceAvailableException.cs
@@ -8,11 +8,35 @@ namespace Brainfuck.Interpreter.Core
 {
     using System;
     using System.Collections.Generic;
+    using System.Runtime.Serialization;
 
     /// <summary>
+    /// Exception thrown by the Interpreter when no Handler is registered to
+    /// request input from.
     /// </summary>
     [Serializable]
     public class NoInputSourceAvailableException : Exception
     {
+        public NoInputSourceAvailableException()
+            : base()
+        {
+        }
+
+        public NoInputSourceAvailableException(String message)
+            : base(message)
+        {
+        }
+
+        public NoInputSourceAvailableException(
+            String message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public NoInputSourceAvailableException(
+            SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
     }
 }
