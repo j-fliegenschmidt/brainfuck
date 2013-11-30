d2879ce279e954173b1218b2b0b5c022dab3eaa1
Janis Fliegenschmidt <janisf@gmx.net>
2013-12-01 00:40:30 +0100

    Added assembly attribute to expose internal types to unit tests. Added unit test of an internal type.

---
+++ BrainfuckInterpreter.Tests/ByteNode_Test.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ByteNode_Test.cs">
+//     Copyright (c) Janis Fliegenschmidt
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Tests.BrainfuckInterpreter
+{
+    using System;
+    using System.Collections.Generic;
+    using Brainfuck.Interpreter.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// </summary>
+    [TestClass]
+    public class ByteNode_Test
+    {
+        [TestMethod]
+        public void SmokeTest()
+        {
+            ByteNode bn = new ByteNode();
+
+            Assert.AreEqual(0, bn.Value);
+
+            bn.IncrementValue();
+
+            Assert.AreEqual(1, bn.Value);
+
+            bn.Next = new ByteNode();
+
+            Assert.IsTrue(bn.HasNext);
+
+            Assert.AreEqual(0, bn.Next.Value);
+
+            bn.Next.IncrementValue();
+
+            Assert.AreEqual(1, bn.Next.Value);            
+        }
+    }
+}
--- BrainfuckInterpreter/Properties/AssemblyInfo.cs
+++ BrainfuckInterpreter/Properties/AssemblyInfo.cs
@@ -14,6 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Making internal classes available to th unit test assemblies.
+[assembly: InternalsVisibleTo("BrainfuckInterpreter.Tests")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
