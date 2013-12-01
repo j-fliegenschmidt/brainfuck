377c6396bee36d034997de3847fc698fc97b613f
Janis Fliegenschmidt <janisf@gmx.net>
2013-12-01 18:21:10 +0100

    Added Project for a Command Line Interpreter

---
+++ CommandLineTool/Program.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs">
+//     Copyright (c) Janis Fliegenschmidt
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace BrainfuckInterpreter.CommandLineTool
+{
+    using System;
+    using System.Collections.Generic;
+    using Brainfuck.Interpreter.Core;
+
+    /// <summary>
+    /// </summary>
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            String program = args[0];
+
+            StringInterpreter interpreter = new StringInterpreter(new InterpreterBase());
+
+            interpreter.OutputAvailable += interpreter_OutputAvailable;
+
+            interpreter.Execute(program);
+
+            Console.ReadLine();
+        }
+
+        static void interpreter_OutputAvailable(String output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
