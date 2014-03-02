a4b777e89c455a9d51efad60faee76ab3e59e9af
Janis Fliegenschmidt <janisf@gmx.net>
2014-03-02 20:29:40 +0100

    Additions to the test project

---
+++ Core.Tests/ByteList_Test.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ByteList_Test.cs">
+//     Copyright (c) Janis Fliegenschmidt
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Brainfuck.Tests.Interpreter.Core
+{
+    using Brainfuck.Interpreter.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// </summary>
+    [TestClass]
+    public class ByteList_Test
+    {
+        [TestMethod]
+        public void SmokeTest()
+        {
+            ByteList bl = new ByteList();
+
+            Assert.AreEqual(0, bl.Value);
+
+            bl.IncrementValue();
+            Assert.AreEqual(1, bl.Value);
+
+            bl.IncrementPointer();
+            Assert.AreEqual(0, bl.Value);
+        }
+    }
+}
--- Core.Tests/ByteNode_Test.cs
+++ Core.Tests/ByteNode_Test.cs
@@ -35,7 +35,67 @@ namespace Brainfuck.Tests.Interpreter.Core
 
             bn.Next.IncrementValue();
 
-            Assert.AreEqual(1, bn.Next.Value);            
+            Assert.AreEqual(1, bn.Next.Value);
+        }
+
+        [TestMethod]
+        public void ValueTest()
+        {
+            ByteNode bn = new ByteNode();
+
+            Assert.AreEqual(0, bn.Value);
+
+            bn.IncrementValue();
+            Assert.AreEqual(1, bn.Value);
+
+            for (int i = 0; i < 20; i++)
+            {
+                bn.IncrementValue();
+            }
+
+            Assert.AreEqual(21, bn.Value);
+
+            bn.DecrementValue();
+            Assert.AreEqual(20, bn.Value);
+
+            for (int i = 0; i < 20; i++)
+            {
+                bn.DecrementValue();
+            }
+
+            Assert.AreEqual(0, bn.Value);
+
+            for (int i = 0; i < byte.MaxValue + 1; i++)
+            {
+                bn.IncrementValue();
+            }
+
+            Assert.AreEqual(0, bn.Value);
+
+            bn.DecrementValue();
+            Assert.AreEqual(byte.MaxValue, bn.Value);
+        }
+
+        [TestMethod]
+        public void LinkTest()
+        {
+            ByteNode bn = new ByteNode();
+
+            Assert.IsFalse(bn.HasNext);
+            Assert.IsNull(bn.Next);
+            Assert.IsFalse(bn.HasPrevious);
+            Assert.IsNull(bn.Previous);
+
+            bn.Next = new ByteNode();
+            Assert.IsTrue(bn.HasNext);
+            Assert.IsNotNull(bn.Next);
+
+            bn.Next.Previous = bn;
+            bn = bn.Next;
+            Assert.IsFalse(bn.HasNext);
+            Assert.IsNull(bn.Next);
+            Assert.IsTrue(bn.HasPrevious);
+            Assert.IsNotNull(bn.Previous);
         }
     }
 }
